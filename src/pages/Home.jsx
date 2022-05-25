5c832f1d3246f7aae12fef0e64ec0615b9f3ea0d
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-25 20:41:23 +0100

    Fixed Homepage Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,31 @@ import Hero from "../components/Hero";
 import Question from "../components/Question";
 import Resources from "../components/Resources";
 import Roadmaps from "../components/Roadmaps";
+import useAuth from "../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const { auth } = useAuth();
   return (
     <>
-      <Hero />
+      <Hero
+        title="Welcome to DevQuests!"
+        subtitle={
+          <>
+            Learn how to code <span className="underline text-lg">Today!</span>{" "}
+            with our Roadmaps and resources!
+          </>
+        }
+        cta={
+          <>
+            {!auth?.username && (
+              <Link to="/register" className="btn btn-success">
+                Get Started
+              </Link>
+            )}
+          </>
+        }
+      />
       <Roadmaps />
       <div className="flex flex-grow flex-col md:flex-row w-full">
         <section className="mt-4 mx-4 bg-base-200 p-10 rounded-box w-full md:w-3/5">
