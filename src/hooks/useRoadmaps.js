065e00ab2208c6f4e3f9eb19b227414f511a83bc
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-25 20:37:09 +0100

    Moved Getting roadmaps to hook to keep context as tidy as possible

--- src/hooks/useRoadmaps.js
+++ src/hooks/useRoadmaps.js
@@ -1,6 +1,34 @@
 import { useContext } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import RoadmapsContext from "../contexts/RoadmapsContext";
+import useAlert from "./useAlert";
+import useAuth from "./useAuth";
+import useAxiosPrivate from "./useAxiosPrivate";
+import axios from "../api/axios";
 
 export default function useRoadmaps() {
-  return useContext(RoadmapsContext);
+  const { roadmaps, setRoadmaps } = useContext(RoadmapsContext);
+  const { auth } = useAuth();
+  const { setAlert } = useAlert();
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const getRoadmaps = async () => {
+    try {
+      if (auth?.username) {
+        const { data } = await axiosPrivate.get("/roadmaps");
+        console.dir(data);
+        setRoadmaps(data);
+      } else {
+        const { data } = await axios.get("/roadmaps");
+        setRoadmaps(data);
+      }
+    } catch (e) {
+      setAlert({ type: "error", message: e.message });
+      navigate("/login", { state: { from: location }, replace: true });
+    }
+  };
+
+  return { roadmaps, setRoadmaps, getRoadmaps };
 }
