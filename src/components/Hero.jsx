278930aacb04b1cda181fd5ff3f8b47a045f8915
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-25 20:18:46 +0100

    Made Hero component reusable for other pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
 
-function Hero() {
-  const { auth } = useAuth();
+function Hero({ title, subtitle, cta }) {
   return (
     <div className="hero min-h-fit bg-base-200 md:place-content-start">
       <div className="hero-content md:ml-40">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Welcome to DevQuests!</h1>
-          <p className="py-6">
-            Learn how to code <span className="underline text-lg">today!</span>{" "}
-            with our Roadmaps and resources!
-          </p>
-          {!auth?.username && (
-            <Link to="/register" className="btn btn-success">
-              Get Started
-            </Link>
-          )}
+        <div>
+          <h1 className="text-5xl font-bold">{title}</h1>
+          <p className="py-6">{subtitle}</p>
+          {cta && cta}
         </div>
       </div>
     </div>
