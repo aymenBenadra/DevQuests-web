c5138e053dc1ffb0ddc4e13f1711891973a14ece
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-05-28 12:41:25 +0100

    Added Dashboard structure

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import SideMenu from "../components/dashboard/SideMenu";
+import Stats from "../components/dashboard/Stats";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <div className="flex items-center md:flex-nowrap flex-wrap justify-center space-y-2 md:space-x-4 px-4 md:space-y-0">
+        <SideMenu />
+        <Outlet />
+        <Stats />
+      </div>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
