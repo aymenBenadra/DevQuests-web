6e2cba9b96c0255a8ac593a4b7cc3a9d70586fa5
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-06-04 10:01:54 +0100

    Added global constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,17 @@
 // Axios API Base URL
 export const BASE_URL = "http://localhost:3001";
+
+export const PAGINATION_ITEMS_COUNT = 6;
+
+export const ROADMAPS_CAROUSEL_COUNT = 3;
+export const ROADMAPS_FILTER_OPTIONS = {
+  all: "All",
+  limited: "Limited",
+
+  notStarted: "Not Started",
+  inProgress: "In Progress",
+  completed: "Completed",
+
+  strict: "Strict",
+  relaxed: "Relaxed",
+};
