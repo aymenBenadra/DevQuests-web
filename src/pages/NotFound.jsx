ec1d18fe65939bafe66bc21e5492439533570ebb
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-06-04 11:32:36 +0100

    Added Not Found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Lost from "../images/404.svg";
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="text-center flex space-y-4 w-full flex-col items-center">
+      <h1 className="font-black text-7xl text-primary">404</h1>
+      <img src={Lost} alt="404" className=" w-1/3" />
+      <p className="text-xl text-primary">
+        Looks like you've lost your way, turn around and continue straight.
+      </p>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
