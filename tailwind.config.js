76a11ba2c1a7aa78f2e41c4245e04a0bb108a42e
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-06-04 10:01:26 +0100

    Added Raleway and Texturina fonts to tailwind default configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
+        serif: ["Texturina", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [require("daisyui")],
 
