1b59b9c071b95e3275da6c6210bb4d3b77adba86
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-06-04 11:32:12 +0100

    Added Questions page

---
+++ src/pages/Questions.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+import Question from "../components/Question";
+import { useQuestions } from "../hooks/Questions";
+import { PAGINATION_ITEMS_COUNT } from "../constants";
+
+function Questions() {
+  const { data: questions, isSuccess } = useQuestions();
+  const [count, setCount] = useState(1);
+  const showMoreRef = useRef();
+
+  function handleShowMore() {
+    if (count * PAGINATION_ITEMS_COUNT < questions.length) {
+      setCount((prev) => prev + 1);
+    }
+  }
+
+  useEffect(() => {
+    if (isSuccess && count * PAGINATION_ITEMS_COUNT >= questions.length) {
+      showMoreRef.current.classList.add("hidden");
+    }
+  }, [isSuccess, count, questions]);
+
+  return (
+    <section className="bg-base-100 p-10 rounded-box w-full">
+      <h1 className="text-4xl mb-6">Questions</h1>
+      <p className="text-primary mb-4">
+        Learn from these Questions now for free!
+      </p>
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 w-full rounded-box p-4">
+        {isSuccess &&
+          questions
+            .slice(0, count * PAGINATION_ITEMS_COUNT)
+            .map((question) => (
+              <Question key={question.id} question={question} />
+            ))}
+      </ul>
+      {isSuccess && (
+        <div className="flex w-full justify-center mt-4">
+          <button
+            className="badge badge-lg badge-primary badge-outline text-center"
+            onClick={handleShowMore}
+            ref={showMoreRef}
+          >
+            Load More
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default Questions;
