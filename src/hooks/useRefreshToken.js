c499a85cce4ec4b98ef08636e82c5b4dc2a07e75
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-06-04 10:32:01 +0100

    Updated Refresh Token hook to handle expired refresh token

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,17 +1,33 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../api/axios";
+import useAlert from "./useAlert";
 import useAuth from "./useAuth";
 
 const useRefreshToken = () => {
   const { setAuth } = useAuth();
+  const { setAlert } = useAlert();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const refresh = async () => {
-    const response = await axios.get("/refresh", {
-      withCredentials: true,
-    });
-    setAuth((prev) => {
-      return { ...prev, accessToken: response.data.accessToken };
-    });
-    return response.data.accessToken;
+    try {
+      const response = await axios.get("/refresh", {
+        withCredentials: true,
+      });
+      setAuth((prev) => {
+        return { ...prev, accessToken: response.data.accessToken };
+      });
+      return response.data.accessToken;
+    } catch (error) {
+      if (error?.response?.status === 403) {
+        setAuth({});
+        setAlert({
+          type: "error",
+          message: "Your session has expired. Please log in again.",
+        });
+        navigate("/login", { state: { from: location }, replace: true });
+      }
+    }
   };
   return refresh;
 };
